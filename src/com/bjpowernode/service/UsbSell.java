2123aee472d19816049d126e695f60eef32c8c0b
XiongZhangHuai <3045663775@qq.com>
2022-02-20 20:00:33 +0800

    3.0

--- src/com/bjpowernode/service/UsbSell.java
+++ src/com/bjpowernode/service/UsbSell.java
@@ -2,4 +2,5 @@ package com.bjpowernode.service;
 
 public interface UsbSell {
     String m();
+
 }
---
+++ src/com/bjpowernode/service/UsbSellImpl.java
@@ -0,0 +1,8 @@
+package com.bjpowernode.service;
+
+public class UsbSellImpl implements UsbSell {
+    @Override
+    public String m() {
+        return null;
+    }
+}
